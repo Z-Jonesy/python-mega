cb8ac66b2b8049eaf8e90d9bfb257ec519d077cd
Z_Jonesy <zoltan.bogdan@gmail.com>
2018-12-25 21:47:58 +0100

    L1_finishing

--- teaching/app.py
+++ teaching/app.py
@@ -1,3 +1,5 @@
+import difflib
+from difflib import get_close_matches
 import json
 data = json.load(open("data.json"))
 
@@ -5,9 +7,20 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+
+    elif len(get_close_matches(w, data.keys())) > 0:
+        a = input("Did you mean %s ?  Y/N " % get_close_matches(w, data.keys())[0])
+        a = a.lower()
+        if a == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif a == "n":
+            return
+        else:
+            return
+
+
     else:
         return "The word does not exists "
 
 w = input("Enter a word: ")
 print(translate(w))
-
